354a0b07238315247108c6550d113614e16daec4
Juan Manuel Rodriguez <juanmanuel.rodriguez@nazaries.com>
2021-06-13 22:50:45 +0200

    Document Dashboard and filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React, { Component, FC } from 'react';
-import { Button } from 'antd';
+import { Component } from 'react';
+import { Layout } from 'antd';
 import { Router } from "react-router-dom";
-import { Header } from './components/header/Header';
-import {createBrowserHistory} from 'history';
+import { Header } from './components/Header';
+import { createBrowserHistory } from 'history';
 import './App.css';
-import Layout, { Content } from 'antd/lib/layout/layout';
-import { SiderComponent } from './components/sider/SiderComponent';
 import { BreadCrumb } from './components/breadcrumb/BreadCrumb';
 import { Routes } from './components/Routes';
+
 class App extends Component {
   history: any;
 
-  constructor(props : any) {
+  constructor(props: any) {
     super(props);
     this.history = createBrowserHistory();
   }
@@ -20,29 +19,15 @@ class App extends Component {
     return (<div className="App">
       <Router history={this.history}>
         <Layout>
-          <Header/>
-          <Routes
-            {...this.props}
-          />
-          <Content style={{ padding: '0 50px' }}>
-            <BreadCrumb/>
-            <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-              <SiderComponent/>
-              <Content
-                className="site-layout-background"
-                style={{
-                  padding: 24,
-                  margin: 0
-                }}
-              >
-                Content
-                  <Button type="primary">Button</Button>
-              </Content>
-            </Layout>
-          </Content>
+          <Header />
+          <Layout.Content style={{ padding: '0 50px' }}>
+            <BreadCrumb />
+            <Routes {...this.props} />
+          </Layout.Content>
         </Layout>
       </Router>
-    </div>)}
+    </div>)
+  }
 }
 
 export default App;
--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -1,20 +1,22 @@
-import { Button } from 'antd';
+import { Button, Layout } from 'antd';
 import logo from '../logo.svg';
 
 export const HomeContainer = () => {
-    return(<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type="primary">Button</Button>
-    </header>);
+  return (
+    <>
+      <Layout>
+        <Layout.Content>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Button
+            type="primary"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">Learn React</Button>
+        </Layout.Content>
+      </Layout>
+    </>
+  );
 }
