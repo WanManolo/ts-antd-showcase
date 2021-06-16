acb4a94e4270a2cd29a39d3475791ce851f8957d
Juan Manuel Rodriguez <juanmanuel.rodriguez@nazaries.com>
2021-06-16 18:09:05 +0200

    Added App tests

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Test Suite for App component
+describe("<App />", () => {
+
+  // Default boilerplate test
+  it('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should contain a visible Header component', async () => {
+    const { findByTestId } = render(<App />);
+    const header = await findByTestId('header-component');
+    expect(header).toBeVisible();
+  });
+
+  it('should contain a visible Breadcrumb component', async () => {
+    const { findByTestId } = render(<App />);
+    const breadcrumb = await findByTestId('breadcrumb-component');
+    expect(breadcrumb).toBeVisible();
+  });
+
 });
