4367087a988460ce0d72f9a8174ea0b4eb987868
Juan Manuel Rodriguez <juanmanuel.rodriguez@nazaries.com>
2021-06-17 08:52:15 +0200

    Test for DocumentDashboard

---
+++ src/test/components/dashboard/DocumentDashboard.test.tsx
@@ -0,0 +1,24 @@
+
+import './matchMedia.mock';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { DocumentDashboard, Props } from '../../../components/dashboard/DocumentDashboard';
+
+// Test fails because of [TypeError: window.matchMedia is not a function]
+// Ref. https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+describe("<DocumentDashboard />", () => {
+
+    function renderDashboard(props: Partial<Props> = {}) {
+        return render(<DocumentDashboard  {...props} />);
+    }
+
+    it('should render a sidebar and a table components', async () => {
+        const { findByTestId } = renderDashboard();
+        const sidebar = await findByTestId('sidebar');
+        expect(sidebar).toBeVisible();
+        const table = await findByTestId('table-component');
+        expect(table).toBeVisible();
+        const button = screen.getByText('Add Document');
+        expect(button).toBeVisible();
+    });
+
+});
